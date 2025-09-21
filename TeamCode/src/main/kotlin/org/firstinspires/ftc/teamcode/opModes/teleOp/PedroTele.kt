66649a1833b1029b32729f1aab9dc8562b2e1d41
Sid <114190535+siddharth-shah121@users.noreply.github.com>
2025-09-21 09:45:43 -0400

    pedro tele component

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/PedroTele.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opModes/teleOp/PedroTele.kt
@@ -1,14 +1,25 @@
 package org.firstinspires.ftc.teamcode.opModes.teleOp
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.nextftc.core.components.SubsystemComponent
 import dev.nextftc.extensions.pedro.PedroComponent
 import dev.nextftc.extensions.pedro.PedroDriverControlled
 import dev.nextftc.ftc.Gamepads
 import dev.nextftc.ftc.NextFTCOpMode
+import dev.nextftc.ftc.components.BulkReadComponent
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants.createFollower
 
 
 @TeleOp(name = "PedroTele")
 class PedroTele : NextFTCOpMode() {
+    init {
+        addComponents(
+            SubsystemComponent(),
+            BulkReadComponent,
+            PedroComponent(Constants::createFollower)
+        )
+    }
     override fun onStartButtonPressed() {
         val driverControlled = PedroDriverControlled(
             Gamepads.gamepad1.leftStickY,
