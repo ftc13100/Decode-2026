6746f202c61689d3d73d4e6f231be6d656f36dc1
AdityaP241 <adi24.patil@gmail.com>
2025-10-12 12:39:28 -0400

    shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/subsystems/Shooter.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/subsystems/Shooter.kt
@@ -1,17 +1,71 @@
 package org.firstinspires.ftc.teamcode.opModes.subsystems
 
+import com.bylazar.configurables.annotations.Configurable
+import com.qualcomm.hardware.limelightvision.Limelight3A
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
 import dev.nextftc.control.KineticState
-import dev.nextftc.control.feedback.FeedbackElement
+import dev.nextftc.control.builder.controlSystem
+import dev.nextftc.control.feedback.PIDCoefficients
+import dev.nextftc.core.subsystems.Subsystem
+import dev.nextftc.ftc.ActiveOpMode
+import dev.nextftc.ftc.ActiveOpMode.telemetry
+import dev.nextftc.hardware.controllable.Controllable
+import dev.nextftc.hardware.impl.MotorEx
+import kotlin.math.cos
 
-object Shooter {
-}
+@Configurable
+object Shooter: Subsystem {
+    lateinit var shooter: MotorEx
 
-class FullPowerFeedback : FeedbackElement {
-    override fun calculate(error: KineticState): Double {
-        return 1.0
+    @JvmField
+    var target = 0.0
+
+    @JvmField
+    var velPIDCoefficients = PIDCoefficients(0.0, 0.0, 0.0)
+
+
+//    val controller = controlSystem {
+//        velPid(velPIDCoefficients)
+//    }
+
+    override fun initialize() {
+        shooter = MotorEx("shooter")
+        shooter.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+
     }
+
+    fun spin() {
+        shooter.power = 0.9
+    }
+
+    fun stop() {
+        shooter.power = 0.0
+    }
+
+
+    fun spinning() {
+        val controller = controlSystem {
+            velPid(velPIDCoefficients)
+        }
+
+        controller.goal = KineticState(
+            velocity = target
+        )
+
+        val output = controller.calculate(
+            KineticState(
+                velocity = shooter.velocity
+            )
+        )
+
+        telemetry.addData("Measured Velocity", shooter.velocity)
+        telemetry.addData("Target Velocity", target)
+
+        telemetry.update()
+
+
+    }
+
 }
 
-controlSystem {
-    feedback(FullPowerFeedback())
-}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/ShooterTeleOp.kt
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.opModes.teleOp
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.nextftc.core.commands.Command
+import dev.nextftc.core.commands.utility.LambdaCommand
+import dev.nextftc.core.components.BindingsComponent
+import dev.nextftc.core.components.SubsystemComponent
+import org.firstinspires.ftc.teamcode.opModes.subsystems.Shooter
+import dev.nextftc.ftc.NextFTCOpMode
+import dev.nextftc.ftc.components.BulkReadComponent
+
+@TeleOp(name = "Shooter Test & Tune")
+class ShooterTeleOp : NextFTCOpMode() {
+    init {
+        SubsystemComponent(
+            Shooter
+        )
+        BulkReadComponent
+        BindingsComponent
+    }
+
+    override fun onInit() {
+
+    }
+}
