dc39bec3cab24b1e7f4391669e73e674b34de451
SachetK <sachet.korada@gmail.com>
2025-12-19 13:27:59 -0500

    Drive Controller object

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/TestTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/TestTeleOp.kt
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.opModes.teleOp
 
+import com.pedropathing.geometry.Pose
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import dev.nextftc.core.commands.CommandManager
 import dev.nextftc.core.components.BindingsComponent
 import dev.nextftc.core.components.SubsystemComponent
 import dev.nextftc.extensions.pedro.PedroComponent
+import dev.nextftc.extensions.pedro.PedroComponent.Companion.follower
 import dev.nextftc.ftc.Gamepads
 import dev.nextftc.ftc.NextFTCOpMode
 import dev.nextftc.ftc.components.BulkReadComponent
@@ -20,5 +21,14 @@ class TestTeleOp : NextFTCOpMode() {
             SubsystemComponent(DriveController),
             PedroComponent(Constants::createFollower),
         )
+    }
+
+    override fun onInit() {
+        follower.pose = Pose(72.0, 72.0, Math.toRadians(90.0))
+        Gamepads.gamepad1.a whenTrue DriveController::pointToTarget
+    }
+
+    override fun onUpdate() {
+        telemetry.update()
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveController.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveController.kt
@@ -1,8 +1,16 @@
 package org.firstinspires.ftc.teamcode.subsystems
 
+import com.pedropathing.geometry.Pose
 import dev.nextftc.core.subsystems.Subsystem
+import dev.nextftc.core.units.rad
+import dev.nextftc.extensions.pedro.PedroComponent.Companion.follower
 import dev.nextftc.extensions.pedro.PedroDriverControlled
+import dev.nextftc.extensions.pedro.TurnTo
+import dev.nextftc.ftc.ActiveOpMode
 import dev.nextftc.ftc.Gamepads
+import org.firstinspires.ftc.teamcode.constants.PoseStorage
+import kotlin.math.PI
+import kotlin.math.atan2
 
 object DriveController: Subsystem {
     override val defaultCommand =
@@ -13,4 +21,21 @@ object DriveController: Subsystem {
         )
             .apply { scalar = 0.9 }
             .requires(this)
+
+    fun pointToTarget() {
+        val target = Pose(3.0, 144.0)
+        val diff = target - follower.pose
+
+        val angle =
+            if (PoseStorage.blueAlliance)
+                atan2(diff.x, diff.y)
+            else
+                PI + atan2(diff.x, diff.y)
+
+        ActiveOpMode.telemetry.addData("Angle Turned", angle.rad.normalized)
+
+        TurnTo(angle.rad.normalized)
+            .requires(this)
+            .schedule()
+    }
 }
