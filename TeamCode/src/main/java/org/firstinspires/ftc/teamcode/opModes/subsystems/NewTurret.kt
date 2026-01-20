3d132566198820a27e526224b30dd1919f74a2cd
SachetK <sachet.korada@gmail.com>
2026-01-20 10:08:36 -0500

    Basic new turret functionality

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/subsystems/NewTurret.kt
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.opModes.subsystems
+
+object NewTurret {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/NewTurretTeleOp.kt
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.opModes.teleOp
+
+class NewTurretTeleOp {
+}
