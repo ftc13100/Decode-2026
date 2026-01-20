e009e3d5298530db0701a011f2820c4958899172
SachetK <sachet.korada@gmail.com>
2026-01-20 10:08:51 -0500

    Basic new turret functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/subsystems/NewTurret.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/subsystems/NewTurret.kt
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.opModes.subsystems
 
-object NewTurret {
+import com.qualcomm.robotcore.hardware.Servo
+import dev.nextftc.core.commands.utility.InstantCommand
+import dev.nextftc.core.subsystems.Subsystem
+import dev.nextftc.ftc.ActiveOpMode
+
+object NewTurret : Subsystem {
+    private lateinit var turret1: Servo
+    private lateinit var turret2: Servo
+
+    var turretPosition = 0.0
+        set(value) {
+            turret1.position = value
+            turret2.position = value
+            field = value
+        }
+    override fun initialize() {
+        turret1 = ActiveOpMode.hardwareMap["turret1"] as Servo
+        turret2 = ActiveOpMode.hardwareMap["turret1"] as Servo
+
+        turretPosition = 0.0
+    }
+
+    val increment = InstantCommand {
+        turretPosition += 0.001
+    }
+
+    val decrement = InstantCommand {
+        turretPosition -= 0.001
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/NewTurretTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/NewTurretTeleOp.kt
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.opModes.teleOp
 
-class NewTurretTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.nextftc.core.components.BindingsComponent
+import dev.nextftc.core.components.SubsystemComponent
+import dev.nextftc.ftc.Gamepads
+import dev.nextftc.ftc.NextFTCOpMode
+import dev.nextftc.ftc.components.BulkReadComponent
+import org.firstinspires.ftc.teamcode.opModes.subsystems.NewTurret
+
+@TeleOp
+class NewTurretTeleOp : NextFTCOpMode() {
+    init {
+        addComponents(
+            SubsystemComponent(NewTurret),
+            BindingsComponent,
+            BulkReadComponent
+        )
+    }
+
+    override fun onInit() {
+        Gamepads.gamepad1.a whenBecomesTrue NewTurret.increment
+        Gamepads.gamepad1.b whenBecomesTrue NewTurret.decrement
+    }
 }
