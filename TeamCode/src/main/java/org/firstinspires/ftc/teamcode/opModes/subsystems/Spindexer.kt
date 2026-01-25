335730d7eaa6d5f50ceb9833a36ee4b829877eab
MomohitPatil <mo24.patil@gmail.com>
2026-01-24 19:14:37 -0500

    started spindexer stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/subsystems/Spindexer.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/subsystems/Spindexer.kt
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.teamcode.opModes.subsystems
 
 import com.bylazar.configurables.annotations.Configurable
+import com.qualcomm.hardware.rev.RevColorSensorV3
+import com.qualcomm.robotcore.hardware.ColorSensor
+import com.qualcomm.robotcore.hardware.Servo
 import com.qualcomm.robotcore.util.ElapsedTime
 import dev.nextftc.control.builder.controlSystem
 import dev.nextftc.control.feedback.PIDCoefficients
 import dev.nextftc.core.subsystems.Subsystem
+import dev.nextftc.ftc.ActiveOpMode
+import dev.nextftc.ftc.ActiveOpMode.hardwareMap
 import dev.nextftc.hardware.impl.MotorEx
+import dev.nextftc.hardware.powerable.SetPower
 
 object Spindexer : Subsystem {
 
@@ -17,6 +23,10 @@ object Spindexer : Subsystem {
     var posPIDCoefficients = PIDCoefficients(0.0, 0.0, 0.0)
 
     val spindexer = MotorEx("spindexer").brakeMode()
+    val color0 = ActiveOpMode.hardwareMap.get(RevColorSensorV3::class.java, "color0")
+    val color1 = ActiveOpMode.hardwareMap.get(RevColorSensorV3::class.java, "color1")
+    val color2 = ActiveOpMode.hardwareMap.get(ColorSensor::class.java, "color2");
+
     private val runtime = ElapsedTime()
 
     val controlSystem = controlSystem {
@@ -24,6 +34,18 @@ object Spindexer : Subsystem {
     }
 
     //For shot
+    val spinShot = SetPower(spindexer,1.0)
+
+    val dexing = intArrayOf(-1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 2, 0, 1,
+        1, 2, 0, 0, 1, 2, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 2, 2, 0, 1, 1, 2, 0, 2, 0, 1, 1, 2, 0, 0,
+        1, 2, 1, 2, 0, 0, 1, 2, 2, 0, 1, 1, 2, 0, 0, 1, 2, 2, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0,
+        0, 0, 1, 0, 0, 0, 1, 1, 2, 0, 0, 1, 2, 2, 0, 1, 2, 0, 1, 1, 2, 0, 0, 1, 2, 2, 0, 1, 1, 2, 0,
+        0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 1, 2, 0, 0, 1, 2, 1, 2, 0, 0, 1, 2, 2, 0, 1, 1,
+        2, 0, 0, 1, 2, 2, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 2, 2, 0, 1, 1, 2, 0, 0, 1, 2, 2, 0,
+        1, 1, 2, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 2, 2, 0, 1, 1, 2, 0, 0, 1, 2, 2, 0, 1, 1, 2, 0,
+        0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0)
+
 
 
 }
