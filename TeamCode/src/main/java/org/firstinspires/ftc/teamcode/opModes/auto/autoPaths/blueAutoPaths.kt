e78473b0c88cffbd7db92396eff0fc572c58758f
Sid <114190535+siddharth-shah121@users.noreply.github.com>
2026-02-27 15:15:19 -0500

    18 3 gate points complete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/auto/autoPaths/blueAutoPaths.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/auto/autoPaths/blueAutoPaths.kt
@@ -8,4 +8,25 @@ import dev.nextftc.core.subsystems.Subsystem
 import dev.nextftc.extensions.pedro.PedroComponent
 
 object blueAutoPaths : Subsystem {
+    val start = Pose(32.325, 135.89, Math.toRadians(180.0))
+    val shoot = Pose(60.0, 84.0, Math.toRadians(-131.0))
+
+    val PGP = Pose(22.0, 60.0, Math.toRadians(-174.0))
+    val PGPcontrol = Pose(41.1728813559322, 61.82033898305083)
+    val PGPback = Pose(60.0, 84.0, Math.toRadians(-155.0))
+
+    val gate = Pose(16.0, 66.0, Math.toRadians(180.0))
+    val eat = Pose(11.0, 51.64, Math.toRadians(135.0))
+    val gateBack = Pose(60.0, 84.0, Math.toRadians(-147.0))
+
+    val PPG = Pose(22.0, 84.0, Math.toRadians(180.0))
+    //uses gateBack for PPG intake
+
+    val leave = Pose(26.0,70.0, Math.toRadians(180.0))
+
+
+
+
+    fun buildPaths() {
+    }
     }
