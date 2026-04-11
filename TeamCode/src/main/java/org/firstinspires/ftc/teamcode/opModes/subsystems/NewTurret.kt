5e3b6ae49836b116edfffd5b48c6885da5420c09
MomohitPatil <mo24.patil@gmail.com>
2026-04-11 12:06:43 -0400

    turret offset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/subsystems/NewTurret.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/subsystems/NewTurret.kt
@@ -36,7 +36,7 @@ object NewTurret : Subsystem {
 
     @JvmField var kVF = 0.0
 
-    const val TURRET_OFFSET = -2.04724
+    const val TURRET_OFFSET = -2.03852
 
     private const val SERVO_MAX_DEG = 300.0 // Servo full travel in degrees
 
